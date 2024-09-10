fa96b998cca54825e9477bdbe19e7f711a7fa2a1
tykkiman <tykkimies@protonmail.com>
2024-09-10 21:59:02 +0300

    n64: specialize compiled blocks
    We'd like the recompiler to take the execution context such as kernel
    mode into account when compiling blocks. That's why it's necessary to
    identify blocks not just by address but all the information used at
    compile time. This is done by packing relevant CPU state to an u32 and
    storing it alongside the block pointer.
    
    The execution state and its representation as bit vector are recomputed
    only when needed, at the moment that means
    
    - each time Context::setMode() is called, happens on powerup,
    - on both MTC0 and MFC0 instructions,
    - on exceptions,
    - when the GDB server requests a block cleanup, and
    - when system state is deserialized ("load state").
    
    Recompiler's block pool is now extended with the state bits and a new
    compiled block cache. The logic is similar to the old implementation:
    a physical address is split into high and low bits, high bits are used
    to index a Pool, and the low bits an individual row inside the pool.
    
    The difference: each row represents the most recently used pair of
    (Block*, state) instead of just a Block*. The recompiler updates a row
    if the state bits don't match by looking up the corresponding block in
    the new compiled block cache, which is just unordered_map<u64, Block*>.
    This is not supposed to happen often, so even a relatively slow STL
    container works fine for the purpose.
    
    Also, there's a new optimization: inlined dual mode instructions.
    The recompiler now emits inline implementations of basic 64-bit ops
    without runtime kernel mode checks. This is made possible by the block
    specialization logic.

--- ares/n64/cpu/cpu.hpp
+++ ares/n64/cpu/cpu.hpp
@@ -883,13 +883,33 @@ struct CPU : Thread {
       u8* code;
     };
 
+    struct PoolRow {
+      Block* block;
+      u32 stateBits;
+    };
+
     struct Pool {
-      Block* blocks[1 << 6];
+      PoolRow* rows[1 << 6];
+    };
+
+    struct JITContext {
+      bool singleInstruction;
+      Context::Endian endian;
+      Context::Mode mode;
+      bool cop1Enabled;
+      bool floatingPointMode;
+      bool is64bit;
+
+      u32  stateBits; //the above state, compressed
+
+      auto update(const CPU& cpu) -> void;
+      auto toBits() const -> u32;
     };
 
     auto reset() -> void {
       pools.resize(1 << 21);  //2_MiB * sizeof(void*) == 16_MiB
       std::ranges::fill(pools, nullptr);
+      blockCache.clear();
     }
 
     auto invalidate(u32 address) -> void {
@@ -905,7 +925,7 @@ struct CPU : Thread {
       auto pool = pools[address >> 8 & 0x1fffff];
       if(!pool) return;
       memory::jitprotect(false);
-      pool->blocks[address >> 2 & 0x3f] = nullptr;
+      pool->rows[address >> 2 & 0x3f] = nullptr;
       memory::jitprotect(true);
       #endif
     }
@@ -921,10 +941,13 @@ struct CPU : Thread {
     }
 
     auto pool(u32 address) -> Pool*;
-    auto block(u64 vaddr, u32 address, bool singleInstruction = false) -> Block*;
+    auto blockCacheKey(u32 address, u32 state) -> u64;
+    auto block(u64 vaddr, u32 address) -> Block*;
 
-    auto emit(u64 vaddr, u32 address, bool singleInstruction = false) -> Block*;
+    auto emit(u64 vaddr, u32 address) -> Block*;
+    auto emitOverflowCheck() -> sljit_jump*;
     auto emitZeroClear(u32 n) -> void;
+    auto checkDualAllowed() -> bool;
     auto emitEXECUTE(u32 instruction) -> bool;
     auto emitSPECIAL(u32 instruction) -> bool;
     auto emitREGIMM(u32 instruction) -> bool;
@@ -936,7 +959,17 @@ struct CPU : Thread {
     bool callInstructionPrologue = false;
     bump_allocator allocator;
     std::vector<Pool*> pools;
+    std::unordered_map<u64, Block*> blockCache;
+    JITContext jitContext;
   } recompiler{*this};
+
+  auto arithmeticOverflowException() -> void {
+    exception.arithmeticOverflow();
+  }
+
+  auto reservedInstructionException() -> void {
+    exception.reservedInstruction();
+  }
 
   struct Disassembler {
     CPU& self;
